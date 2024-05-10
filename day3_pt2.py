d0fc46bd0bf7a9fbb29aaaf2de3496ee65c7f2e4
James Barnes <62391527+freddytums@users.noreply.github.com>
2024-05-10 09:59:35 -0500

    Update day3_pt2.py

--- day3_pt2.py
+++ day3_pt2.py
@@ -3,8 +3,6 @@ from aoc_utility import *
 input = ingestTextFile("input/day3_input.txt")
 output = 0
 
-
-
 def isInbounds(rowIdx, colIdx):
     if rowIdx < 0 or rowIdx > 139:
         return False
@@ -12,56 +10,55 @@ def isInbounds(rowIdx, colIdx):
         return False  
     return True
 
-def searchForAdjacentNums(SpecialChar_rowIdx, SpecialChar_colIdx, validNumberMap: list):
-    countOfAdjactentNums = 0
-    numbers = [[],[]]
+def numberGrab(searchRow, searchCol):
+    number = []
+    columnOffset = 1
+    lookingForFirstDigit = True
+    while lookingForFirstDigit:
+        if isInbounds(searchRow, searchCol - columnOffset):
+            if input[searchRow][searchCol - columnOffset].isnumeric():
+                columnOffset += 1
+            else:
+                lookingForFirstDigit = False
+        else:
+            lookingForFirstDigit = False
+    grabbingNumber = True
+    while grabbingNumber:
+        if isInbounds(searchRow, searchCol - columnOffset + 1):
+            if input[searchRow][searchCol - columnOffset + 1].isnumeric():
+                number.append(input[searchRow][searchCol - columnOffset + 1])
+
+                temp_input = list(input[searchRow])
+                temp_input[searchCol - columnOffset + 1] = '.'
+                input[searchRow] = temp_input
+
+                columnOffset -= 1
+            else:
+                grabbingNumber = False
+        else:
+            grabbingNumber = False
+
+    return int(''.join(number))
+        
+def search(rowIdx, colIdx):
+    global output
+    numbersFound = 0
+    numbers = [0, 0]
     for rowOffset in range(3):
         for colOffset in range(3):
-            rowIdx = SpecialChar_rowIdx - 1 + rowOffset
-            colIdx = SpecialChar_colIdx - 1 + colOffset
-            
-            if isInbounds(rowIdx, colIdx) and not validNumberMap[rowIdx][colIdx]:
-                print("Row: " + str(rowIdx) + " Col: " + str(colIdx))
-                countOfAdjactentNums += 1
-                if input[rowIdx][colIdx].isnumeric():
-                    numbers[countOfAdjactentNums].append(input[rowIdx][colIdx])
-                    validNumberMap[rowIdx][colIdx] = True
+            searchRow = rowIdx + rowOffset - 1; searchCol = colIdx + colOffset - 1
+            if isInbounds(searchRow, searchCol):
+                if input[searchRow][searchCol].isnumeric():
+                    numbers[numbersFound] = numberGrab(searchRow, searchCol)
+                    numbersFound += 1
 
-                    searchingLeft = True
-                    i = 1
-                    while searchingLeft:
-                        if isInbounds(rowIdx, colIdx - i):
-                            if input[rowIdx][colIdx - i].isnumeric():
-                                validNumberMap[rowIdx][colIdx - i] = True
-                                numbers[countOfAdjactentNums].insert(0, input[rowIdx][colIdx])
-                            else:
-                                searchingLeft = False
-                        else:
-                            searchingLeft = False
-                        i += 1
+    if numbersFound == 2:
+        output += numbers[0] * numbers[1]
 
-                    searchingRight = True
-        
-                    i = 1
-                    while searchingRight:
-                        if isInbounds(rowIdx, colIdx + i):
-                            if input[rowIdx][colIdx + i].isnumeric():
-                                validNumberMap[rowIdx][colIdx + i] = True
-                                numbers[countOfAdjactentNums].append(input[rowIdx][colIdx])
-                            else:
-                                searchingRight = False
-                        else:
-                            searchingRight = False
-                        i += 1
-    print(numbers)
-    return validNumberMap
-
-validNumberMap = [[False] * len(input[0]) for i in range(len(input))]
-specialCharacters = "*"
 
 for rowIdx, line in enumerate(input):
     for colIdx, char in enumerate(line):
-        if char in specialCharacters:
-            validNumberMap = searchForAdjacentNums(rowIdx, colIdx, validNumberMap)
+        if char == '*':
+            search(rowIdx, colIdx)
 
 print("Day 3 - Part 2: " + str(output))
