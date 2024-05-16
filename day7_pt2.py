9fbaa95144bd3c28c76041ce4ceff93b4d239358
James Barnes <62391527+freddytums@users.noreply.github.com>
2024-05-16 16:08:38 -0500

    day 7

---
+++ day7_pt2.py
@@ -0,0 +1,114 @@
+from aoc_utility import *
+
+from operator import *
+
+debug = False
+test = False
+
+input = ingestTextFile(("input/day7_input.txt", "input/day7_test_input.txt")[test])
+output = 0
+
+cardRank = 'J23456789TQKA'
+
+def largest(array):
+    n = len(array)
+    array[cardValue('J')] = 0
+    sortedArray = array.copy()
+    sortedArray.sort()
+    number = sortedArray[n-1]
+    index = array.index(number)
+    return number, index
+
+def cardValue(card: str):
+    try:
+        return (cardRank.index(card) + 1)
+    except:
+        return 0
+
+def type(hand: str):
+    cardCounts = [0] * (len(cardRank) + 1)
+
+    for card in hand:
+        cardCounts[cardValue(card)] += 1
+    
+    largestCount, largestCountIdx = largest(cardCounts)
+
+    countJ = hand.count('J')
+    cardCounts[largestCountIdx] += countJ  
+
+    if 5 in cardCounts:
+        return 6
+    elif 4 in cardCounts:
+        return 5
+    elif 3 in cardCounts:
+        if 2 in cardCounts:
+            return 4
+        else:
+            return 3
+    elif countOf(cardCounts, 2) == 2:
+        return 2
+    elif 2 in cardCounts:
+        return 1
+    else:
+        return 0
+
+class Game():
+    def __init__(self, gameData: str):
+        self.hand = gameData.split()[0]
+        self.bid = int(gameData.split()[1])
+        self.type = type(self.hand)
+
+gamesRankedList = []
+
+for line in input:
+    game = Game(line)
+
+    if debug:
+        print(game.hand)
+        print(type(game.hand))
+        print("\n")
+
+    if len(gamesRankedList) == 0:
+        gamesRankedList.append(
+            [game.hand, game.bid, type(game.hand)]
+        )
+
+    for i, gameRanked in enumerate(gamesRankedList):
+        if (game.type > gameRanked[2]) and (i == (len(gamesRankedList) - 1)):
+            gamesRankedList.append(
+                [game.hand, game.bid, type(game.hand)]
+            )
+            break
+        elif game.type < gameRanked[2]:
+            gamesRankedList.insert(
+                i,
+                [game.hand, game.bid, type(game.hand)]
+            )
+            break
+        elif game.type == gameRanked[2]:
+            for idx, card in enumerate(game.hand):
+                finished = False
+                cardA_value = cardValue(card)
+                cardB_value = cardValue(gameRanked[0][idx])
+
+                if cardA_value > cardB_value:
+                    if i == (len(gamesRankedList) - 1):
+                        gamesRankedList.append(
+                            [game.hand, game.bid, type(game.hand)]
+                        )
+                        finished = True
+                    break
+                elif cardA_value < cardB_value:
+                    gamesRankedList.insert(
+                        i,
+                        [game.hand, game.bid, type(game.hand)]
+                    )
+                    finished = True
+                    break
+            if finished:
+                break
+
+for idx, game in enumerate(gamesRankedList):
+    output += (game[1] * (idx + 1))  
+
+print("Day 7 - Part 2: " + str(output))
